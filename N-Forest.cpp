99e98c4fdda4396ac28ae64010f9572baecefeb2
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2024-06-02 21:06:52 +0530

    Create N-Forest.cpp

---
+++ N-Forest.cpp
@@ -0,0 +1,20 @@
+void nForest(int n) {
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+// Problem statement
+// Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+// For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Output: 
+// * * *
+// * * *
+// * * *
